2f94f4f237ec3d4208986dbdb24735f295fa4b2a
Endowmissy <adeolaayo31@yahoo.com>
2019-09-25 02:37:47 +0100

    made some changes project and fix server and connection issue

--- Finance_api/models/users.js
+++ Finance_api/models/users.js
@@ -1,5 +1,4 @@
-const mongoose = require('mongoose');
-
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: {
--- Finance_api/routes/users.js
+++ Finance_api/routes/users.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import keys from '../config/keys';
-
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const keys = require("../config/keys");
+
 // Load input validation
 const validateRegisterInput = require("../validation/register");
 const validateLoginInput = require("../validation/login");
